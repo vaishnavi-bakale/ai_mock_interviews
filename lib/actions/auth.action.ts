05a84e50c9f4b86dd5a464a06f30187022ff65a8
Vaishnavi Bakale <co.2022.vsbakale@bitwardha.ac.in>
2025-10-14 14:40:17 +0530

    fix unterminated block comment

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,19 +2,17 @@
 
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
-// Session duration (1 week)
+
 const SESSION_DURATION = 60 * 60 * 24 * 7;
 
-// Set session cookie
+
 export async function setSessionCookie(idToken: string) {
   const cookieStore = await cookies();
 
-  // Create session cookie
   const sessionCookie = await auth.createSessionCookie(idToken, {
     expiresIn: SESSION_DURATION * 1000, // milliseconds
   });
 
-  // Set cookie in the browser
   cookieStore.set("session", sessionCookie, {
     maxAge: SESSION_DURATION,
     httpOnly: true,
@@ -28,7 +26,7 @@ export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
 
   try {
-    // Check if user already exists
+
     const userRecord = await db.collection("users").doc(uid).get();
     if (userRecord.exists) {
       return {
@@ -37,7 +35,6 @@ export async function signUp(params: SignUpParams) {
       };
     }
 
-    // Save new user to Firestore
     await db.collection("users").doc(uid).set({
       name,
       email,
@@ -93,7 +90,6 @@ export async function signIn(params: SignInParams) {
   }
 }
 
-// Sign out user by clearing the session cookie
 export async function signOut() {
   const cookieStore = await cookies();
   cookieStore.delete("session");
@@ -124,7 +120,6 @@ export async function getCurrentUser(): Promise<User | null> {
   }
 }
 
-// Check if user is authenticated
 export async function isAuthenticated() {
   try {
     const user = await getCurrentUser();
