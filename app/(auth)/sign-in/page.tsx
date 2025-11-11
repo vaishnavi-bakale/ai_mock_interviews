0c87111009bbf9a803bda02156ae3dfc518b8b02
Vaishnavi Bakale <co.2022.vsbakale@bitwardha.ac.in>
2025-11-11 18:20:35 +0530

    updated logic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,15 @@
-
 export const dynamic = 'force-dynamic';
 
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import AuthForm from "@/components/AuthForm";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  // If signed in already ➜ redirect home
+  if (user) redirect("/");
+
   return <AuthForm type="sign-in" />;
 };
 
