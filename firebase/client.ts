b5f9547b251235ff561a264e4212da341dc728fe
Vaishnavi Bakale <co.2022.vsbakale@bitwardha.ac.in>
2025-11-16 23:20:04 +0530

    update change

--- firebase/client.ts
+++ firebase/client.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig): getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig): getApp();
 
 export const auth = getAuth(app);
 //export const db = getFirestore(app);
